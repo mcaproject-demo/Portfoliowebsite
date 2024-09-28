1f28188f57e076b6fb4396bca634b4cef0810e63
shwetha <shwethath.mca@cambridge.edu.in>
2024-09-28 09:15:03 +0530

    created java file

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 public class index{
   public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println("Welcome to jenkins");
+    System.out.println("Welcome");
   }
 }
